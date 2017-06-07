fd397f8075412b723aa094f14e191aa4520c84bb
ohjh0721 <ohjh0721@gmail.com>
2017-06-07 10:13:34 -0700

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,15 @@
+# Assignment2_Q2
+
+c)
+
+      int max(int arr[], int start, int end) {                                                        	
+            int largestIndex = start;
+            if(start >  end) {
+                      return largestIndex;
+            }	
+            if(arr[largestIndex] < arr[start+1]) {
+                    largestIndex = start+1;
+            }
+            max(arr, largest, end);
+            return largestIndex;			
+       
